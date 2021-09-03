7633d987b66abf85ce173cd4c4e887b7df4a50c3
LASMAR Raya <59080542+Lasmar-Raya@users.noreply.github.com>
2021-09-03 11:01:26 +0100

    Add files via upload

---
+++ admin/inc/header.php
@@ -0,0 +1,12 @@
+
+<div id='header'>
+        <div id='logo'>
+                <h2>Admin Section</h2>
+        </div>
+        <div id='title'>
+                <h1>Admin Panel</h1>
+        </div>
+        <div id='logout'>
+                <h3><a href="admin_logout.php">Logout</a></h3>
+        </div>
+</div>
