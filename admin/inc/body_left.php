20eeb1ad8d25ca4ee689ccf90c2915063a5a63a8
LASMAR Raya <59080542+Lasmar-Raya@users.noreply.github.com>
2021-09-03 10:56:50 +0100

    Add files via upload

--- admin/inc/body_left.php
+++ admin/inc/body_left.php
@@ -1,1 +1,62 @@
+<div id="body_left">
 
+    <h3>ADMIN</h3>
+    <div style="height:250px; background-image:url(../assets/images/admin.png);background-size:cover; background-position:center" ></div>
+    
+    <h3>Content Management</h3>
+    <ul>
+        <li><a href="index.php?raya">View Modules</a></li> 
+        <li><a href="index.php?ines">View courses</a></li>
+    </ul>
+    
+    <h3>User Management</h3>
+    <ul>
+        <li><a href="index.php?member">View All Students</a></li>
+        <li><a href="index.php?teacher">View All Teachers</a></li>
+    </ul>
+    
+    <h3>Pages Management</h3>
+    <ul>
+        <li><a href="index.php?terms">Terms & conditions Page</a></li>
+        <li><a href="index.php?contact">Contact our Page</a></li>
+        <li><a href="index.php?about">About our page</a></li>
+        <li><a href="index.php?faqs">FAQs Page</a></li>
+    </ul>
+
+</div>
+
+<?php
+    if(isset($_GET['raya'])){
+        include './category.php';
+    }
+
+    if(isset($_GET['ines'])){
+        include './sub_category.php';
+    }
+
+    if(isset($_GET['semestre1']) || isset($_GET['semestre2']) || isset($_GET['semestre3']) ||isset($_GET['semestre4']) ||isset($_GET['semestre5']))
+    { include 'semestre.php';}
+    
+    if(isset($_GET['member'])){
+        include './members.php';
+    }
+
+    if(isset($_GET['terms'])){
+        include './terms.php';
+    }
+
+    if(isset($_GET['contact'])){
+        include './contact.php';
+    }
+
+    if(isset($_GET['about'])){
+        include './about.php';
+    }
+
+    if(isset($_GET['faqs'])){
+        include './faqs.php';
+    }
+    if(isset($_GET['teacher'])){
+        include './teacher.php';
+    }
+?>
---
+++ admin/inc/body_right.php
@@ -0,0 +1,7 @@
+<?php if(!isset($_GET['raya']) && !isset($_GET['ines']) && !isset($_GET['member']) && 
+!(isset($_GET['semestre1']) || isset($_GET['semestre2']) || isset($_GET['semestre3']) ||isset($_GET['semestre4']) ||isset($_GET['semestre5'])) && 
+!(isset($_GET['terms'])) && !isset($_GET['contact']) && !isset($_GET['about']) && !isset($_GET['faqs'])&& !isset($_GET['teacher'])){?>
+<div id='body_right'>
+    <h3>OverView</h3>
+</div>
+<?php  } ?>
